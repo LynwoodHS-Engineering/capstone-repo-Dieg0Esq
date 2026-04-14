186cd5e80a58e96b7732d74202e9965f780266d9
Dieg0Esq <dieesq4062@stu.mylusd.org>
2026-04-13 19:41:51 -0700

    esp32_ps5_controller.ino

--- Code/esp32_ps5_controller.ino
+++ Code/esp32_ps5_controller.ino
@@ -1,26 +1,22 @@
-// ESP32-WROOM - PS5 Tank Controller
-// Reads PS5 left joystick and sends commands to Arduino
-// Library: ps5Controller by rodneyBakiskan
-// Wiring: ESP32 GPIO17 (TX2) -> Arduino Pin 0 (RX)
-//         ESP32 GPIO16 (RX2) -> Arduino Pin 1 (TX)
-//         ESP32 GND          -> Arduino GND
-// Controls: Left stick up=Forward, down=Backward
-//           Left stick left=Left, right=Right
-
 #include <ps5Controller.h>
+#include <LiquidCrystal.h>
 
 #define TX_PIN 17
 #define RX_PIN 16
-#define ENCODER_A 34
-#define ENCODER_B 35
+#define ENCODER_A 32
+#define ENCODER_B 33
+
+LiquidCrystal lcd(19, 18, 4, 5, 2, 21);
 
 const int DEAD_ZONE = 30;
 const char* PS5_MAC = "58:10:31:21:65:D7";
 
 char lastCmd = 'S';
+char lastTurret = 'Z';
 volatile long pulseCount = 0;
 unsigned long lastTime = 0;
 float speed = 0;
+bool wasConnected = false;
 
 void IRAM_ATTR countPulse() {
   pulseCount++;
@@ -30,18 +26,31 @@ void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
   ps5.begin(PS5_MAC);
-  pinMode(ENCODER_A, INPUT);
-  pinMode(ENCODER_B, INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A), countPulse, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_B), countPulse, RISING);
+  pinMode(ENCODER_A, INPUT_PULLUP);
+  pinMode(ENCODER_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), countPulse, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_B), countPulse, FALLING);
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("PS5: Waiting... ");
+  lcd.setCursor(0, 1);
+  lcd.print("RPM: 0          ");
+
   Serial.println("Tank Controller Started");
-  Serial.println("--------------------");
 }
 
 void sendCmd(char cmd) {
   if (cmd != lastCmd) {
     Serial2.write(cmd);
     lastCmd = cmd;
+  }
+}
+
+void sendTurret(char cmd) {
+  if (cmd != lastTurret) {
+    Serial2.write(cmd);
+    lastTurret = cmd;
   }
 }
 
@@ -53,25 +62,46 @@ void loop() {
     Serial.print("Speed: ");
     Serial.print(speed);
     Serial.println(" RPM");
-    Serial.println("--------------------");
+    lcd.setCursor(0, 1);
+    lcd.print("RPM: ");
+    lcd.print(speed);
+    lcd.print("     ");
     pulseCount = 0;
     lastTime = currentTime;
   }
 
   if (!ps5.isConnected()) {
+    if (wasConnected) {
+      lcd.setCursor(0, 0);
+      lcd.print("PS5: Disconnected");
+      wasConnected = false;
+    }
     sendCmd('S');
+    sendTurret('Z');
     delay(100);
     return;
   }
 
-  int leftX = ps5.LStickX();
-  int r2    = ps5.R2Value();
-  int l2    = ps5.L2Value();
+  if (!wasConnected) {
+    lcd.setCursor(0, 0);
+    lcd.print("PS5: Connected  ");
+    wasConnected = true;
+  }
+
+  int leftX  = ps5.LStickX();
+  int rightX = ps5.RStickX();
+  int rightY = ps5.RStickY();
+  int r2     = ps5.R2Value();
+  int l2     = ps5.L2Value();
 
   bool movingForward  = r2 > 10;
   bool movingBackward = l2 > 10;
   bool turningRight   = leftX >  DEAD_ZONE;
   bool turningLeft    = leftX < -DEAD_ZONE;
+  bool turretRight    = rightX >  DEAD_ZONE;
+  bool turretLeft     = rightX < -DEAD_ZONE;
+  bool turretUp       = rightY >  DEAD_ZONE;
+  bool turretDown     = rightY < -DEAD_ZONE;
 
   if (turningRight) {
     sendCmd('R');
@@ -83,6 +113,18 @@ void loop() {
     sendCmd('B');
   } else {
     sendCmd('S');
+  }
+
+  if (turretRight) {
+    sendTurret('Y');
+  } else if (turretLeft) {
+    sendTurret('X');
+  } else if (turretUp) {
+    sendTurret('U');
+  } else if (turretDown) {
+    sendTurret('D');
+  } else {
+    sendTurret('Z');
   }
 
   delay(50);
